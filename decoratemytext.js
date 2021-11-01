047aa3bbc2c5da46d096ee7524ed8999eb84dbc4
YENEALEM-FL <57996562+YENEALEM-FL@users.noreply.github.com>
2021-11-01 13:26:33 -0500

    bug fixed

--- decoratemytext.js
+++ decoratemytext.js
@@ -8,6 +8,7 @@ document.getElementById("ta1").style.background="blue";
 document.getElementById("ta1").style.color="red";
 document.getElementById("ta1").style.weight=normal;
 document.getElementById("ta1").style.textDecoration ='none';
+document.getElementsByTagName("body")[0].style.backgroundColor="white";
 
 }
 var newSize;
@@ -16,6 +17,7 @@ function FormatInTimerInterval(){
 	var currSize=window.getComputedStyle(document.getElementById("ta1")).fontSize;
 	newSize=parseInt(currSize)+2+"pt";
 	document.getElementById("ta1").style.fontSize=newSize;
+	//document.getElementsByTagName("body")[0].style.backgroundColor="white";
 	textFormat();
 	
 	}
@@ -32,6 +34,7 @@ function newTextFormat() {
 		document.getElementById("ta1").style.background="yellow";
 		document.getElementById("ta1").style.color="green";
 		document.getElementById("ta1").style.textDecoration = 'underline';
+		document.getElementsByTagName("body")[0].style.backgroundImage="url('images/background.png')";
 		
 	}
 	else{
